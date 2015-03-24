b8bdbbe0174ada1d5cc198385f75ec974c044f72
Atanas Dragolov <adragolov@gmail.com>
2015-03-24 18:50:44 +0200

    Fix issues related to versioning.

--- Example/Pods/Target Support Files/Pods-saorsa.PXQSpinner/Pods-saorsa.PXQSpinner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_saorsa_PXQSpinner : NSObject
-@end
-@implementation PodsDummy_Pods_saorsa_PXQSpinner
-@end
--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  saorsa.PXQSpinnerTests.m
-//  saorsa.PXQSpinnerTests
-//
-//  Created by Atanas Dragolov on 03/24/2015.
-//  Copyright (c) 2014 Atanas Dragolov. All rights reserved.
-//
-
-${TEST_EXAMPLE}
